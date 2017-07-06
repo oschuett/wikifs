3f19c9153c9d0ef750399aa788cbd0f2787f0f9a
Ole Schuett <ole.schuett@empa.ch>
2017-07-06 15:37:43 +0200

    add sys-prefix

--- reinstall.sh
+++ reinstall.sh
@@ -5,8 +5,8 @@ jupyter nbextension install   --sys-prefix --py --symlink jupyter_wiki
 jupyter nbextension enable    --sys-prefix --py jupyter_wiki
 jupyter nbextension list
 
-jupyter serverextension disable --py jupyter_wiki
-jupyter serverextension enable  --py jupyter_wiki
+jupyter serverextension disable --sys-prefix --py jupyter_wiki
+jupyter serverextension enable  --sys-prefix --py jupyter_wiki
 jupyter serverextension list
 
 #EOF
