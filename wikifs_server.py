14c0951e9f6745436c727ecffaeeb900405ea038
Ole Schuett <ole.schuett@empa.ch>
2017-07-06 16:11:35 +0200

    convert server into flask blueprint

--- wikifs_server.py
+++ wikifs_server.py
@@ -3,17 +3,22 @@
 import os
 import json
 import subprocess
-from base64 import b64encode, b64decode
-
 from functools import wraps
-from flask import Flask, render_template, json, request, abort, redirect, url_for, flash, session
-app = Flask(__name__)
+from base64 import b64encode, b64decode
+from flask import Flask, current_app, Blueprint, request, abort
+
+wikifs_blueprint = Blueprint('wikifs_server', __name__)
+userdb = None
+
+#===============================================================================
+def wikifs_root():
+    return current_app.config['WIKIFS_ROOT']
 
 #===============================================================================
 def to_full_path(path):
     if path.startswith("/"):
         path = path[1:]
-    return os.path.join(app.config['WIKIFS_ROOT'], path)
+    return os.path.join(wikifs_root(), path)
 
 #===============================================================================
 def token_required(func):
@@ -25,7 +30,7 @@ def token_required(func):
             return abort(401)
         token = request.headers["Authorization"]
 
-        if token not in userdb:
+        if not userdb or token not in userdb.keys():
             reload_userdb() # reload userdb and check again.
             if token not in userdb:
                 return abort(401)
@@ -37,12 +42,12 @@ def token_required(func):
 #===============================================================================
 def reload_userdb():
     global userdb
-    fn = os.path.join(app.config['WIKIFS_ROOT'], "userdb.json")
+    fn = os.path.join(wikifs_root(), "userdb.json")
     print("reloading user database from: "+fn)
     userdb = json.load(open(fn))
 
 #===============================================================================
-@app.route('/wikifs/getattr')
+@wikifs_blueprint.route('/getattr')
 @token_required
 def api_getattr():
     path = request.args["path"]
@@ -63,7 +68,7 @@ def api_getattr():
     return json.dumps(answer)
 
 #===============================================================================
-@app.route('/wikifs/chmod', methods=['POST'])
+@wikifs_blueprint.route('/chmod', methods=['POST'])
 @token_required
 def api_chmod():
     path = request.args["path"]
@@ -80,7 +85,7 @@ def api_chmod():
     #TODO handle executable bit properly (might require a commit)
 
 #===============================================================================
-@app.route('/wikifs/create')
+@wikifs_blueprint.route('/create')
 @token_required
 def api_create():
     path = request.args["path"]
@@ -95,7 +100,7 @@ def api_create():
     return json.dumps({})
 
 #===============================================================================
-@app.route('/wikifs/remove')
+@wikifs_blueprint.route('/remove')
 @token_required
 def api_remove():
     path = request.args["path"]
@@ -113,7 +118,7 @@ def api_remove():
     return json.dumps({})
 
 #===============================================================================
-@app.route('/wikifs/rename', methods=['POST'])
+@wikifs_blueprint.route('/rename', methods=['POST'])
 @token_required
 def api_rename():
     old_path = request.args["path"]
@@ -138,7 +143,7 @@ def api_rename():
     return json.dumps({})
 
 #===============================================================================
-@app.route('/wikifs/readdir')
+@wikifs_blueprint.route('/readdir')
 @token_required
 def api_readdir():
     path = request.args["path"]
@@ -150,7 +155,7 @@ def api_readdir():
     return(json.dumps(entries))
 
 #===============================================================================
-@app.route('/wikifs/download')
+@wikifs_blueprint.route('/download')
 @token_required
 def api_download():
     path = request.args["path"]
@@ -170,7 +175,7 @@ def api_download():
 
 
 #===============================================================================
-@app.route('/wikifs/upload', methods=['POST'])
+@wikifs_blueprint.route('/upload', methods=['POST'])
 @token_required
 def api_upload():
     path = request.args["path"]
@@ -244,7 +249,7 @@ def git_commit_file(path):
     commit_msg = None
     if git_file_tracked(path):
         cmd = ["git", "diff-index", "--quiet", "HEAD", full_path]
-        has_changed = subprocess.call(cmd, cwd=app.config['WIKIFS_ROOT'])
+        has_changed = subprocess.call(cmd, cwd=wikifs_root())
         if has_changed != 0:
             commit_msg = "Edit "+path
     else:
@@ -252,15 +257,15 @@ def git_commit_file(path):
 
     if commit_msg:
         author = '--author="'+current_user['git_author']+'"'
-        subprocess.check_call(["git", "add", full_path], cwd=app.config['WIKIFS_ROOT'])
-        subprocess.check_call(["git", "commit", author, "-m", commit_msg], cwd=app.config['WIKIFS_ROOT'])
+        subprocess.check_call(["git", "add", full_path], cwd=wikifs_root())
+        subprocess.check_call(["git", "commit", author, "-m", commit_msg], cwd=wikifs_root())
 
 #===============================================================================
 def git_file_tracked(path):
     full_path = to_full_path(path)
     cmd = ["git", "ls-files", "--error-unmatch", full_path]
     devnull = open("/dev/null", "w")
-    file_tracked = subprocess.call(cmd, stdout=devnull, stderr=devnull, cwd=app.config['WIKIFS_ROOT'])
+    file_tracked = subprocess.call(cmd, stdout=devnull, stderr=devnull, cwd=wikifs_root())
     return file_tracked == 0
 
 #===============================================================================
@@ -269,8 +274,8 @@ def git_remove_file(path):
     if git_file_tracked(path):
         commit_msg = "Remove "+path
         author = '--author="'+current_user['git_author']+'"'
-        subprocess.check_call(["git", "rm", "-f", full_path], cwd=app.config['WIKIFS_ROOT'])
-        subprocess.check_call(["git", "commit", author, "-m", commit_msg], cwd=app.config['WIKIFS_ROOT'])
+        subprocess.check_call(["git", "rm", "-f", full_path], cwd=wikifs_root())
+        subprocess.check_call(["git", "commit", author, "-m", commit_msg], cwd=wikifs_root())
     else:
         os.remove(full_path)
 
@@ -281,8 +286,8 @@ def git_rename_file(old_path, new_path):
     if git_file_tracked(old_path):
         commit_msg = "Rename "+old_path+" -> "+new_path
         author = '--author="'+current_user['git_author']+'"'
-        subprocess.check_call(["git", "mv", "-f", old_full_path, new_full_path], cwd=app.config['WIKIFS_ROOT'])
-        subprocess.check_call(["git", "commit", author, "-m", commit_msg], cwd=app.config['WIKIFS_ROOT'])
+        subprocess.check_call(["git", "mv", "-f", old_full_path, new_full_path], cwd=wikifs_root())
+        subprocess.check_call(["git", "commit", author, "-m", commit_msg], cwd=wikifs_root())
     else:
         print("rename: "+old_full_path + " -> "+new_full_path)
         os.rename(old_full_path, new_full_path)
@@ -294,8 +299,9 @@ if __name__ == "__main__":
         print("Usage: app <wikifs_root>")
         sys.exit(255)
 
+    app = Flask(__name__)
+    app.register_blueprint(wikifs_blueprint, url_prefix='/wikifs')
     app.config['WIKIFS_ROOT'] = os.path.realpath(sys.argv[1])
-    reload_userdb()
     app.run(port=5002, debug=True)
 
 #EOF
